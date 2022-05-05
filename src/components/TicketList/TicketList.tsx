a65efd004926cfcb2f3932fe57923ae2770cf46e
Ni <ninelamericanel@gmail.com>
2022-05-05 12:22:59 +0300

    create form for filter and modified jsx

--- src/components/TicketList/TicketList.tsx
+++ src/components/TicketList/TicketList.tsx
@@ -109,7 +109,6 @@ const TicketList: FC = () => {
   ];
 
   const elements = arr.map((item, id) => {
-    console.log(item);
     return (
       <li key={id} className="ticket">
         <Ticket />
@@ -117,7 +116,41 @@ const TicketList: FC = () => {
     );
   });
 
-  return <ul className="list">{elements}</ul>;
+  return (
+    <>
+      <div className="filter">
+        <form>
+          <label>КОЛИЧЕСТВО ПЕРЕСАДОК</label>
+          <label>
+            <input type="checkbox"></input>
+            Все
+          </label>
+          <label>
+            <input type="checkbox"></input>
+            Без пересадок
+          </label>
+          <label>
+            <input type="checkbox"></input>1 пересадка
+          </label>
+          <label>
+            <input type="checkbox"></input>2 пересадки
+          </label>
+          <label>
+            <input type="checkbox"></input>3 пересадки
+          </label>
+        </form>
+      </div>
+      <div className="main">
+        <div className="tabs">
+          <button className="btn">Самый дешевый</button>
+          <button className="btn">Самый быстрый</button>
+          <button className="btn">Оптимальный</button>
+        </div>
+        <ul className="list">{elements}</ul>
+        <button className="btn">Показать еще 5 билетов</button>
+      </div>
+    </>
+  );
 };
 
 export default TicketList;
