40ba5424c61474b870c4c81f78a6d8432ec9430f
Ni <ninelamericanel@gmail.com>
2022-05-05 10:09:27 +0300

    added basic style and imported

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import { TicketList } from '../TicketList';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <TicketList />
     </div>
   );
