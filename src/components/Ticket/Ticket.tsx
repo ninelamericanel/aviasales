5275e4b6a5a634d2a2269b945342f21c213b41c1
Ni <ninelamericanel@gmail.com>
2022-05-06 13:42:35 +0300

    fixed structure component and added styles

--- src/components/Ticket/Ticket.tsx
+++ src/components/Ticket/Ticket.tsx
@@ -2,40 +2,49 @@ import React, { FC } from 'react';
 
 import classes from './Ticket.module.scss';
 import logo from './S7Logo.png';
+
 const Ticket: FC = () => {
   return (
     <>
       <div className={classes.header}>
-        <div className="price">
+        <div className={classes.price}>
           <p>13 400 P</p>
         </div>
         <div className="logo">
           <img src={logo}></img>
         </div>
       </div>
-      <table>
-        <tr>
-          <th>MOW - HKT</th>
-          <th>В пути</th>
-          <th>2 пересадки</th>
-        </tr>
-        <tr>
-          <td>10:45 - 08:00</td>
-          <td>21ч 15м</td>
-          <td>HKG, JNB</td>
-        </tr>
+      <table className={classes.table}>
+        <thead>
+          <tr>
+            <td>MOW - HKT</td>
+            <td>В пути</td>
+            <td>2 пересадки</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>10:45 - 08:00</td>
+            <td>21ч 15м</td>
+            <td>HKG, JNB</td>
+          </tr>
+        </tbody>
       </table>
-      <table>
-        <tr>
-          <th>MOW - HKT</th>
-          <th>В пути</th>
-          <th>2 пересадки</th>
-        </tr>
-        <tr>
-          <td>10:45 - 08:00</td>
-          <td>21ч 15м</td>
-          <td>HKG, JNB</td>
-        </tr>
+      <table className={classes.table}>
+        <thead>
+          <tr>
+            <th>MOW - HKT</th>
+            <th>В пути</th>
+            <th>2 пересадки</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>10:45 - 08:00</td>
+            <td>21ч 15м</td>
+            <td>HKG, JNB</td>
+          </tr>
+        </tbody>
       </table>
     </>
   );
