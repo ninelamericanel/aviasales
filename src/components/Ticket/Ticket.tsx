2e8c1506188c38a97886a897aef7a5aa9baa0c06
Ni <ninelamericanel@gmail.com>
2022-05-06 13:16:15 +0300

    modified components, create ticket structure

--- src/components/Ticket/Ticket.tsx
+++ src/components/Ticket/Ticket.tsx
@@ -1,17 +1,43 @@
 import React, { FC } from 'react';
 
-// import classes from './Ticket.module.scss';
+import classes from './Ticket.module.scss';
 import logo from './S7Logo.png';
 const Ticket: FC = () => {
   return (
-    <div>
-      <div className="price">
-        <p>13 400 P</p>
+    <>
+      <div className={classes.header}>
+        <div className="price">
+          <p>13 400 P</p>
+        </div>
+        <div className="logo">
+          <img src={logo}></img>
+        </div>
       </div>
-      <div className="logo">
-        <img src={logo}></img>
-      </div>
-    </div>
+      <table>
+        <tr>
+          <th>MOW - HKT</th>
+          <th>В пути</th>
+          <th>2 пересадки</th>
+        </tr>
+        <tr>
+          <td>10:45 - 08:00</td>
+          <td>21ч 15м</td>
+          <td>HKG, JNB</td>
+        </tr>
+      </table>
+      <table>
+        <tr>
+          <th>MOW - HKT</th>
+          <th>В пути</th>
+          <th>2 пересадки</th>
+        </tr>
+        <tr>
+          <td>10:45 - 08:00</td>
+          <td>21ч 15м</td>
+          <td>HKG, JNB</td>
+        </tr>
+      </table>
+    </>
   );
 };
 
