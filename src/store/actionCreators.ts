18eb03f938215f2d62f6041b74a9c8056fb12854
ninelamericanel <ninelamericanel@gmail.com>
2022-06-17 15:01:50 +0300

    created action creators

---
+++ src/store/actionCreators.ts
@@ -0,0 +1,4 @@
+export const click = (id: number) => ({ type: 'CLICK', id });
+export const checkActionCreator = (id: number) => ({ type: 'CHECK', id });
+export const checkAllActionCreator = () => ({ type: 'CHECK_ALL' });
+export const checkAutomaticActionCreator = () => ({ type: 'CHECK_AUTOMATIC' });
