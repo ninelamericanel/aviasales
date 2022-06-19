34a66a5308be4fef4b2225b8dad54b6e4fc21942
ninelamericanel <ninelamericanel@gmail.com>
2022-06-19 13:13:53 +0300

    added state for tabs and realized event

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,15 +1,45 @@
 import React, { FC } from 'react';
+import { connect } from 'react-redux';
+import { Dispatch, bindActionCreators } from 'redux';
+
+import { StateType, TabsType } from 'types';
+import * as actions from 'store/actionCreators';
 
 import classes from './Tabs.module.scss';
 
-const Tabs: FC = () => {
+interface Props {
+  tabs: TabsType[];
+  click: (id: number) => void;
+}
+
+const Tabs: FC<Props> = ({ tabs, click }) => {
   return (
     <div className={classes.tabs}>
-      <button className={classes.btn}>САМЫЙ ДЕШЕВЫЙ</button>
-      <button className={classes.btn}>САМЫЙ БЫСТРЫЙ</button>
-      <button className={classes.btn}>ОПТИМАЛЬНЫЙ</button>
+      {/*<button className={classes.btnActive}>dd</button>*/}
+      {/*<button className={classes.btn}>sss</button>*/}
+      {/*<button className={classes.btn}>ss</button>*/}
+      {tabs.map((tab) => {
+        return (
+          <button key={tab.id} className={tab.isActive ? classes.btnActive : classes.btn} onClick={() => click(tab.id)}>
+            {tab.name}
+          </button>
+        );
+      })}
     </div>
   );
 };
 
-export default Tabs;
+const mapStateToProps = (state: StateType) => {
+  return {
+    tabs: state.TabsReducer.tabs,
+  };
+};
+
+const mapDispatchToProps = (dispatch: Dispatch) => {
+  const { clickActionCreator } = bindActionCreators(actions, dispatch);
+  return {
+    click: (id: number) => clickActionCreator(id),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Tabs);
---
+++ src/store/reducers/TabsReducer.ts
@@ -0,0 +1,31 @@
+import { Action, ActionType } from 'actionTypes';
+import { TabsType } from 'types';
+
+interface InitialState {
+  tabs: TabsType[];
+}
+
+const initialState: InitialState = {
+  tabs: [
+    { id: 1, name: 'САМЫЙ ДЕШЕВЫЙ', isActive: false },
+    { id: 2, name: 'САМЫЙ БЫСТРЫЙ', isActive: false },
+    { id: 3, name: 'ОПТИМАЛЬНЫЙ', isActive: false },
+  ],
+};
+
+const TabsReducer = (state = initialState, action: Action) => {
+  const { tabs } = state;
+  switch (action.type) {
+    case ActionType.CLICK:
+      return {
+        ...state,
+        tabs: tabs.map((item) =>
+          item.id === action.id ? { ...item, isActive: !item.isActive } : { ...item, isActive: false }
+        ),
+      };
+    default:
+      return state;
+  }
+};
+
+export default TabsReducer;
