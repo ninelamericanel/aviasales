8623341f6fe767cd84c9f98bed39e62bea082ce9
ninelamericanel <ninelamericanel@gmail.com>
2022-06-20 16:58:19 +0300

    created function for getting tickets from server

--- src/services/ticketServices.ts
+++ src/services/ticketServices.ts
@@ -1,7 +1,11 @@
-// export default class TicketServices {
-
 export const getSearchId = async () => {
   const response = await fetch('https://aviasales-test-api.kata.academy/search');
   return response.json();
 };
-// }
+
+export const getTickets = async () => {
+  const response = await fetch(
+    'https://aviasales-test-api.kata.academy/tickets?searchId=4d29fc545c5f1670c083229c30cd9d3a'
+  );
+  return response.json();
+};
