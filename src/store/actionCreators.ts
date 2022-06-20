b3c7e56c45ee40cbd5ed06595ab0f109ee299921
ninelamericanel <ninelamericanel@gmail.com>
2022-06-20 17:01:20 +0300

    created action creator for set tickets list

--- src/store/actionCreators.ts
+++ src/store/actionCreators.ts
@@ -1,6 +1,9 @@
 import { ActionType } from '../actionTypes';
+import { TicketType } from '../types';
 
 export const clickActionCreator = (id: number) => ({ type: ActionType.CLICK, id });
 export const checkActionCreator = (id: number) => ({ type: ActionType.CHECK, id });
 export const checkAllActionCreator = () => ({ type: ActionType.CHECK_ALL });
 export const checkAutomaticActionCreator = () => ({ type: ActionType.CHECK_AUTOMATIC });
+
+export const setTicketsActionCreator = (tickets: TicketType[]) => ({ type: ActionType.SET_TICKETS, tickets });
