8ee46614c8fd09c66b8d425e542febdae8604fea
ninelamericanel <ninelamericanel@gmail.com>
2022-06-20 18:30:15 +0300

    added new reducer

--- src/store/reducers/reducer.ts
+++ src/store/reducers/reducer.ts
@@ -3,7 +3,8 @@ import { combineReducers } from 'redux';
 import FilterReducer from './FilterReducer';
 import TabsReducer from './TabsReducer';
 import TicketsReducer from './TicketsReducer';
+import SearchIdReducer from './SearchIdReducer';
 
-const reducer = combineReducers({ TabsReducer, FilterReducer, TicketsReducer });
+const reducer = combineReducers({ TabsReducer, FilterReducer, TicketsReducer, SearchIdReducer });
 
 export default reducer;
