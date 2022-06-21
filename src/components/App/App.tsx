9ca299f9f605589941ce32cf2604d629dd7b00fd
ninelamericanel <ninelamericanel@gmail.com>
2022-06-21 18:30:21 +0300

    added loader and alert message, added mapDispatchToProps and realized useEffect for getting tickets from api

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,20 +1,37 @@
 import React, { FC, useEffect } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators, Dispatch } from 'redux';
+import { Spin, Alert } from 'antd';
+import 'antd/dist/antd.css';
 
-import { getSearchId } from 'services/ticketServices';
 import { TicketList } from 'components/TicketList';
 import { Tabs } from 'components/Tabs';
 import { Filter } from 'components/Filter';
+import { getSearchId } from 'services/ticketServices';
+import * as thunks from 'store/thunks';
+import { StateType } from 'types';
 
 import classes from './App.module.scss';
 import logo from './Logo.png';
 
-const App: FC = () => {
+interface Props {
+  getTickets: (searchId: string) => void;
+  load: boolean;
+  error: boolean;
+}
+
+const App: FC<Props> = ({ getTickets, error, load }) => {
   useEffect(() => {
-    getSearchId().then((r) => {
-      const session = localStorage.searchId ? localStorage.searchId : r.searchId;
-      localStorage.setItem('searchId', session);
+    getSearchId().then((result) => {
+      getTickets(result.searchId);
     });
   }, []);
+
+  const loader = load ? <Spin className={classes.spinner} size="large" /> : null;
+  const errorView = error ? (
+    <Alert message="Error" description="Ошибка! попробуйте позже" type="error" showIcon />
+  ) : null;
+  const view = !load && !error ? <TicketList tickets={[]} /> : null;
 
   return (
     <div className={classes.app}>
@@ -26,10 +43,24 @@ const App: FC = () => {
       <main className={classes.main}>
         <Tabs />
         <Filter />
-        <TicketList />
+        {loader || errorView || view}
       </main>
     </div>
   );
 };
 
-export default App;
+const mapStateToProps = (state: StateType) => {
+  return {
+    load: state.TicketsReducer.load,
+    error: state.TicketsReducer.error,
+  };
+};
+
+const mapDispatchToProps = (dispatch: Dispatch) => {
+  const { getTicketsThunk } = bindActionCreators(thunks, dispatch);
+  return {
+    getTickets: getTicketsThunk,
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
