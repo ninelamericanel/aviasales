95fa9ad517003aaa36808b2a6696ae1b1d122408
ninelamericanel <ninelamericanel@gmail.com>
2022-06-22 13:06:06 +0300

    deleted

--- src/store/reducers/reducer.ts
+++ src/store/reducers/reducer.ts
@@ -3,8 +3,7 @@ import { combineReducers } from 'redux';
 import FilterReducer from './FilterReducer';
 import TabsReducer from './TabsReducer';
 import TicketsReducer from './TicketsReducer';
-import SearchIdReducer from './SearchIdReducer';
 
-const reducer = combineReducers({ TabsReducer, FilterReducer, TicketsReducer, SearchIdReducer });
+const reducer = combineReducers({ TabsReducer, FilterReducer, TicketsReducer });
 
 export default reducer;
